1f53b2d22b628000416424bea64f4a33d9806485
Kenn Dean <krdean@digitalinksec.com>
2025-07-17 21:23:16 -0400

    Update Program.cs

--- src/02_05b/Program.cs
+++ src/02_05b/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.SemanticKernel;
+
+namespace _02_05b
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var modelDeploymentName = "gpt-4.1";
+            var azureOpenAIEndpoint = "https://dis-openai-0705.openai.azure.com/";
+            var azureOpenAIKey = "DJiBCSANPd42dZGBApT4G6CeLD4gRSBeU0KmajSBSXzFbiUFY94SJQQJ99BGACHYHv6XJ3w3AAABACOG1pXm";
+
+            var builder = Kernel.CreateBuilder();
+            builder.Services.AddAzureOpenAIChatCompletion(
+                modelDeploymentName,
+                azureOpenAIEndpoint,
+                azureOpenAIKey
+            );
+
+            var kernel = builder.Build();
+
+            var topic = "The Semantic Kernel SDK has been born on December 19th ";
+            var prompt = $"Generate a very short funny poem about the given event Event:{topic}";
+
+            var result = await kernel.InvokePromptAsync(prompt);
+
+            Console.WriteLine(result);
+        }
+    }
+}
